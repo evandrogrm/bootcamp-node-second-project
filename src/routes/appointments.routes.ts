c27ed95465077f686c60478e448b97e3cd3fba36
Evandro Gaspar <evandro.gaspar@digitalpages.com.br>
2020-06-18 21:39:21 -0300

    Adjusted metadata for models; Connected TypeORM with repository/service; Added reflect-metadata

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,30 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  return response.json(appointmentRepository.all());
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository);
+  return response.json(await appointmentRepository.find());
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService(
-      appointmentRepository,
-    );
+    const createAppointmentService = new CreateAppointmentService();
 
-    return response.json(
-      createAppointmentService.execute({ provider, date: parsedDate }),
-    );
+    const appointment = await createAppointmentService.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
   } catch (error) {
     return response.status(400).json({ message: error.message });
   }
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
@@ -8,16 +9,12 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-  private appointmentRepository: AppointmentRepository;
+  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository);
 
-  constructor(appointmentRepository: AppointmentRepository) {
-    this.appointmentRepository = appointmentRepository;
-  }
-
-  public execute({ provider, date }: RequestDTO): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
       appointmentDate,
     );
 
@@ -25,10 +22,13 @@ class CreateAppointmentService {
       throw new Error('Esse horário já foi escolhido');
     }
 
-    const appointment = this.appointmentRepository.create({
+    const appointment = await appointmentRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentRepository.save(appointment);
+
     return appointment;
   }
 }
